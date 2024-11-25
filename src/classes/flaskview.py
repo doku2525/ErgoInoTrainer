ac945f75760a25cb044f0ff37529ec3a8a3c6e0e
doku2525 <dirkwodesu4649@gmail.com>
2024-11-25 10:26:55 +0100

    In flaskview.py die einfachen Kommandos in einer route_map zusammengefasst und route_builder() implementiert. Alle Tests ausser test_viewdatenmodell.py OK!

--- src/classes/flaskview.py
+++ src/classes/flaskview.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from flask import Flask, jsonify, render_template, Request, request, redirect, Response
 from threading import Thread
 import logging as lg
@@ -19,6 +20,20 @@ class FlaskView:
         self.daten = ViewDatenmodell()
         self.web_kommando = None
 
+        self.web_kommando_mappings = {
+            'pause': 'PAUSE',
+            'musik_mute': 'MUSIK_MUTE',
+            'pwm_plusplus': 'PWM++',
+            'pwm_plus': 'PWM+',
+            'pwm_minusminus': 'PWM--',
+            'pwm_minus': 'PWM-',
+            'pause_nach_inhalt': 'PAUSE_NACH_INHALT',
+            'change_trainigsprogramm_unendlich': 'CHANGE_TRANINGSPROGRAMM_UNENDLICH'
+        }
+        self.register_kommando_routes()
+        self.define_manual_routes()
+
+    def define_manual_routes(self):
         # Route für die HTML-Datei
         @self.app.route('/')
         @self.app.route('/index')
@@ -29,59 +44,31 @@ class FlaskView:
         def get_data():
             return jsonify(asdict(self.daten))
 
-        # Routen mit den Befehlen
-        @self.app.route('/pause')
-        def pause() -> Response:
-            self.browser_key = 'PAUSE'
+    def register_kommando_routes(self) -> None:
+        """Registriert dynamisch einfache Routen."""
+        for route, command in self.web_kommando_mappings.items():
+            self.app.add_url_rule(f'/{route}', route, self.create_kommando_route_handler(command))
+
+    def create_kommando_route_handler(self, command: str) -> Callable[[], Response]:
+        """Erstellt einen Routen-Handler für einen bestimmten Befehl."""
+        def handler() -> Response:
+            self.browser_key = command
             return redirect(request.referrer)
 
-        @self.app.route('/musik_mute')
-        def musik_mute() -> Response:
-            self.browser_key = 'MUSIK_MUTE'
-            return redirect(request.referrer)
+        return handler
 
-        @self.app.route('/pwm_plusplus')
-        def pwm_plusplus() -> Response:
-            self.browser_key = 'PWM++'
-            return redirect(request.referrer)
-
-        @self.app.route('/pwm_plus')
-        def pwm_plus() -> Response:
-            self.browser_key = 'PWM+'
-            return redirect(request.referrer)
-
-        @self.app.route('/pwm_minus')
-        def pwm_minus() -> Response:
-            self.browser_key = 'PWM-'
-            return redirect(request.referrer)
-
-        @self.app.route('/pwm_minusminus')
-        def pwm_minusminus() -> Response:
-            self.browser_key = 'PWM--'
-            return redirect(request.referrer)
-
-        @self.app.route('/pause_nach_inhalt')
-        def pause_nach_inhalt() -> Response:
-            self.browser_key = 'PAUSE_NACH_INHALT'
-            return redirect(request.referrer)
-
-        @self.app.route('/change_trainigsprogramm_unendlich')
-        def change_trainigsprogramm_unendlich() -> Response:
-            self.browser_key = 'CHANGE_TRANINGSPROGRAMM_UNENDLICH'
-            return redirect(request.referrer)
-
-    def update(self, daten_modell):
+    def update(self, daten_modell: ViewDatenmodell) -> None:
         """Aktualisiert die anzuzeigenden Daten."""
         self.daten = daten_modell
 
-    def draw_daten(self):
+    def draw_daten(self) -> None:
         pass
 
-    def run(self):
+    def run(self) -> None:
         """Startet den Flask-Server in einem separaten Thread."""
         self.app.run(debug=False, use_reloader=False, host='0.0.0.0')
 
-    def start_server(self):
+    def start_server(self) -> None:
         """Initialisiert und startet den Server im Hintergrund."""
         thread = Thread(target=self.run)
         thread.daemon = True
