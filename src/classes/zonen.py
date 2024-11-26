b46be0ad2672aede7ee2d88becfc71609c490dad
doku2525 <dirkwodesu4649@gmail.com>
2024-11-26 12:35:39 +0100

    zonen.py Kommentare hinzugefuegt. Alle Tests ausser test_viewdatenmodell.py OK!

--- src/classes/zonen.py
+++ src/classes/zonen.py
@@ -7,32 +7,35 @@ if TYPE_CHECKING:
 
 class Zonen:
     def __init__(self):
-        self.zonen = {}
-        self.tachoWerte = {'dist': [0, 0, 0], 'zeit': [0, 0, 0], 'herz': [0, 0, 0]}
-        self.daten = {}
-        self.__pwm = 0
+        self.zonen = {}     # dict[pwmwert]['zeit'|'dist'|'herz][bisherGesamt, aktuellerStart]
+        self.tachoWerte = {'dist': [0, 0, 0], 'zeit': [0, 0, 0], 'herz': [0, 0, 0]}   # Anscheinend wird nur [0] benutzt
+        self.daten = {}     # Wird offensichtlich ueberhaupt nicht verwendet
+        self.__pwm = 0      # letzter PWM-Wert
 
     def updateZone(self, pwm: int, zeit: FlexibleZeit, dist: int, herz: int = 0) -> None:
         lokale_zeit = zeit.als_s()
         self.updateTacho(lokale_zeit, dist, herz)
         if len(self.zonen.keys()) == 0:
             self.__pwm = pwm
-        if pwm not in self.zonen.keys():
+        if pwm not in self.zonen.keys():  # Bisher unbekannter PWM-Wert
             self.zonen[pwm] = {'zeit': [0, lokale_zeit], 'dist': [0, dist], 'herz': [0, herz]}
-        if self.__pwm == pwm:
+        if self.__pwm == pwm:       # PWM hat sich seit letztem Aufruf nicht veranedert
             self.tachoWerte['zeit'][0] = lokale_zeit
             self.tachoWerte['dist'][0] = dist
             self.tachoWerte['herz'][0] = herz
-        else:
+        else:                       # PWM hat sich veranedert und die Werte des vorherigen PWM muss angepasst werden.
+            # Berechne die Werte fuer den alten PWM-Wert
+            # Position 2 beinhaltet die Werte bei Beginn Position 1 die bisher in dem PWM-Bereich gefahrenen Werte
             self.zonen[self.__pwm]['zeit'][0] += lokale_zeit - self.zonen[self.__pwm]['zeit'][1]
             self.zonen[self.__pwm]['dist'][0] += dist - self.zonen[self.__pwm]['dist'][1]
             self.zonen[self.__pwm]['herz'][0] += herz - self.zonen[self.__pwm]['herz'][1]
             if self.zonen[self.__pwm]['zeit'][0] == 0:
                 self.zonen.pop(self.__pwm, None)
+            # Setze die hintere (zonen[][][1]) Position auf die aktuellen Werte
             self.zonen[pwm]['zeit'][1] = lokale_zeit
             self.zonen[pwm]['dist'][1] = dist
             self.zonen[pwm]['herz'][1] = herz
-            self.__pwm = pwm
+            self.__pwm = pwm        # Setze den letzten PWM-Wert auf aktuellen PWM-Wert
 
     def updateTacho(self, zeit_in_s, dist, herz):
         self.tachoWerte['zeit'][0] = zeit_in_s
