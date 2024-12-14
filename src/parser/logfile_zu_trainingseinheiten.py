1826a89d582926497cd6016627c9fa72ccdfd134
doku2525 <dirkwodesu4649@gmail.com>
2024-12-14 22:14:30 +0100

    Parser Intervalle extrahiert Alle Tests OK!

--- src/parser/logfile_zu_trainingseinheiten.py
+++ src/parser/logfile_zu_trainingseinheiten.py
@@ -51,8 +51,45 @@ def parse_trainingslog(dateiname: str) -> dict:
 
     return result
 
+def extrahiere_intervalle_fuer_training(dateiname: str, trainings_string: str) -> dict:
+    with open(dateiname, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        start_zeit = datetime.datetime.strptime('0:00:01', '%H:%M:%S').time()
+        result = {}
+        for zeile in reader:
+            obj = LogZeile(*zeile)
+
+            # training, *_ = zeile
+            trainingszeit = datetime.datetime.strptime(obj.dauer, '%H:%M:%S').time()
+            # print(zeile)
+
+            schluessel = f"{obj.datum} : {obj.name}"
+            if trainingszeit == start_zeit and schluessel == trainings_string:
+                result = result | {schluessel: []}
+            if schluessel == trainings_string and obj.inhalt == 'Intervall':
+                result = result | {
+                    schluessel: result.get(schluessel, []) + [
+                            f"{obj.intervall_countdown}\t{obj.pwm}\t{obj.cad}\t{obj.cad_avg_intervall}"]}
+
+    return result
+
+def drucke_jeden_intervall(dateiname, training):
+    for elem in parse_trainingslog(LOG_FILE).keys():
+        if elem.split(' : ')[1] == training:
+            for datum, zeiten in extrahiere_intervalle_fuer_training(LOG_FILE, elem).items():
+                print(f"{datum}")
+                for zeile in zeiten:
+                    print(f"\t{zeile}")
 
 if __name__ == '__main__':
 
-    for elem in parse_trainingslog(LOG_FILE).keys():
-        print(f"{elem}")
+#    for elem in parse_trainingslog(LOG_FILE).keys():
+#        print(f"{elem.split(' : ')[1]}")
+
+    drucke_jeden_intervall(LOG_FILE, 'G1 mit 15sek Sprints')
+#    for elem in parse_trainingslog(LOG_FILE).keys():
+#        print(f"{elem.split(' : ')[1]}")
+#    for datum, zeiten in extrahiere_intervalle_fuer_training(LOG_FILE,'2024-12-08 : G1 mit 15sek Sprints').items():
+#        print(f"{datum}")
+#        for zeile in zeiten:
+#            print(f"\t{zeile}")
